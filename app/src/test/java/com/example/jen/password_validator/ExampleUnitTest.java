5ffee969528b9776d9955d7879216ab0352b8ee3
Jen <jenna.mcneil@dal.ca>
2017-05-27 20:27:58 -0300

    Commit a2-stage2

--- app/src/test/java/com/example/jen/password_validator/ExampleUnitTest.java
+++ app/src/test/java/com/example/jen/password_validator/ExampleUnitTest.java
@@ -20,6 +20,11 @@ public class ExampleUnitTest extends passwordValidator{
         validate1("Hello");
         validate1("greetHin435");
     }
+    public void validateCorrect2() throws Exception {
+        validate2("Hello");
+          validate2("greetHin435");
+    }
+
 
 
 }
